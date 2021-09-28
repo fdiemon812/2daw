27b77eac77749c8b70053fd0af75b6bb7baa8759
flavio <flavio.dediego@gmail.com>
2021-09-28 21:01:59 +0200

    avanzando js

--- FRONTEND/ws/Eventos2/ejercicio2.js
+++ FRONTEND/ws/Eventos2/ejercicio2.js
@@ -1,8 +1,8 @@
 
-
+let borrar= document.getElementById("borrar");
 let boton= document.getElementById("boton");
 boton.addEventListener("click", addEmpleado);
-
+borrar.addEventListener("click", delEmpleado);
 
 function addEmpleado(){
 
@@ -25,25 +25,65 @@ function addEmpleado(){
     let numeroEmpleado= tabla.getElementsByTagName("tr").length
 
 
-
     nombre2.innerHTML = nombre;
     apellido2.innerHTML = apellido;
     dni2.innerHTML = dni;
     idEmpleado.innerHTML = numeroEmpleado;
 
+    let comprobador=0;
+    for(i =0; i<numeroEmpleado;i++){
+        
 
-    fiNueva.appendChild(idEmpleado);
-    fiNueva.appendChild(dni2);
-    fiNueva.appendChild(nombre2);
-    fiNueva.appendChild(apellido2);
+        if(tabla.getElementsByTagName("tr")[i].querySelector("td:nth-child(2)").innerText == dni ){
+
+            comprobador=comprobador+1;
+        }
+    }
+    
+    if(comprobador==0){
+
+        
+        fiNueva.appendChild(idEmpleado);
+        fiNueva.appendChild(dni2);
+        fiNueva.appendChild(nombre2);
+        fiNueva.appendChild(apellido2);
+    
+        tabla.appendChild(fiNueva)
+        
+    
+        let cabecera= document.querySelector("p:first-of-type")
+    
+        cabecera.innerHTML= "NUMERO TOTAL DE EMPLEADOS:  "+numeroEmpleado;
+        
+
+    }else{
+        alert("El dni ya existe")
+    }
 
 
 
-    tabla.appendChild(fiNueva)
 
 
-    let cabecera= document.querySelector("p:first-of-type")
+}
 
-    cabecera.innerHTML= "NUMERO TOTAL DE EMPLEADOS:  "+numeroEmpleado;
 
+function delEmpleado(){
+
+    let dniABorrar= document.getElementById("dni").value
+    let numEmpleados = document.getElementById("tab").getElementsByTagName("tr").length
+    let filas=document.getElementById("tab").getElementsByTagName("tr");
+
+    for(i=0;i<numEmpleados;i++){
+
+        let columna= (filas[i].querySelector("td:nth-child(2)")).innerText
+
+        if(columna == dniABorrar){
+
+            (document.getElementById("tab")).deleteRow(i)
+
+        }
+
+
+
+    }
 }
